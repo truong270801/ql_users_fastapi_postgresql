9b7e95adf695ee114d5fabacfb7edfc3d28c171a
truong270801 <tranvantruong27082001@gmail.com>
2024-05-03 11:26:23 +0700

    chore:Change database connection import type

--- database/database.py
+++ database/database.py
@@ -6,7 +6,13 @@ import os
 
 load_dotenv()
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_name}"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = declarative_base()
